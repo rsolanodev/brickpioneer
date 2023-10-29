370d9c38851ac6ab1c68fab52832f177456821ee
rsolanoweb <rubensoljim@gmail.com>
2023-10-29 21:12:29 +0100

    feat(models): remove fields

--- brickpioneer/models.py
+++ brickpioneer/models.py
@@ -3,10 +3,10 @@ import attrs
 
 @attrs.define
 class Promotion:
-    developer: str = attrs.field()
-    province: str = attrs.field()
-    city: str = attrs.field()
-    name: str = attrs.field()
-    url: str = attrs.field()
-    price_min: str = attrs.field()
+    developer: str
+    province: str
+    city: str
+    name: str
+    url: str
+    price_min: str
     is_available: bool = True
